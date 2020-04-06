463e1393fe1d4e7b7584deeba6063b255583c024
Gaurav Pandey <gaurav.pandey@cyara.com>
2020-04-06 09:18:46 +0530

    Sonar test - 3

--- teamcity-integration/Controllers/WeatherForecastController.cs
+++ teamcity-integration/Controllers/WeatherForecastController.cs
@@ -11,7 +11,6 @@ namespace Eshop.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        string test;
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
